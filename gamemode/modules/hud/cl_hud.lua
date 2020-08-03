80bdb4af061f338fc5dd1afbb0b1e9d215016798
Tassilo <tassia710@gmail.com>
2020-08-03 15:36:20 +0200

    Improved sounds

--- gamemode/modules/hud/cl_hud.lua
+++ gamemode/modules/hud/cl_hud.lua
@@ -377,8 +377,22 @@ Display notifications
 ---------------------------------------------------------------------------]]
 local function DisplayNotify(msg)
     local txt = msg:ReadString()
-    GAMEMODE:AddNotify(txt, msg:ReadShort(), msg:ReadLong())
-    surface.PlaySound("buttons/lightswitch2.wav")
+    local class = msg:ReadShort()
+    GAMEMODE:AddNotify(txt, class, msg:ReadLong())
+
+    if class == NOTIFY_GENERIC then
+        surface.PlaySound("ambient/water/drip2.wav")
+    elseif class == NOTIFY_ERROR then
+        surface.PlaySound("buttons/button10.wav")
+    elseif class == NOTIFY_UNDO then
+        surface.PlaySound("buttons/button17.wav")
+    elseif class == NOTIFY_HINT then
+        surface.PlaySound("buttons/bell1.wav")
+    elseif class == NOTIFY_CLEANUP then
+        surface.PlaySound("buttons/button15.wav")
+    else
+    	surface.PlaySound("buttons/lightswitch2.wav")
+    end
 
     -- Log to client console
     MsgC(Color(255, 20, 20, 255), "[DarkRP] ", Color(200, 200, 200, 255), txt, "\n")
