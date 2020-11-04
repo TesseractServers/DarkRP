8a7135a40e015f09fbebb37bebdf92cf93a2e1dc
Nephew04 <63836061+Nephew04@users.noreply.github.com>
2020-11-04 21:21:59 +0100

    Fixed Chatsystem again

--- gamemode/modules/chat/cl_chat.lua
+++ gamemode/modules/chat/cl_chat.lua
@@ -8,11 +8,10 @@ end
 Add a message to chat
 ---------------------------------------------------------------------------]]
 local function AddToChat(bits)
-    local col1 = Color(241, 172, 101)
+    local col1 = Color(net.ReadUInt(8), net.ReadUInt(8), net.ReadUInt(8))
 
     local prefixText = net.ReadString()
     local ply = net.ReadEntity()
-	local name = ply:Nick()
     ply = IsValid(ply) and ply or LocalPlayer()
 
     if not IsValid(ply) then return end
@@ -22,7 +21,7 @@ local function AddToChat(bits)
         prefixText = prefixText ~= "" and prefixText or ply:SteamName()
     end
 
-    local col2 = Color(255, 255, 151)
+    local col2 = Color(net.ReadUInt(8), net.ReadUInt(8), net.ReadUInt(8))
 
     local text = net.ReadString()
     local shouldShow
@@ -32,13 +31,13 @@ local function AddToChat(bits)
         end
 
         if shouldShow ~= true then
-            chat.AddNonParsedText(col1, name, col2, ":" .. text)
+            chat.AddNonParsedText(Color(241, 172, 101, 255), prefixText, Color(255, 255, 151, 255), ": " .. text)
         end
     else
         shouldShow = hook.Call("ChatText", GAMEMODE, "0", prefixText, prefixText, "darkrp")
 
         if shouldShow ~= true then
-            chat.AddNonParsedText(col1, prefixText)
+            chat.AddNonParsedText(Color(241, 172, 101, 255), prefixText)
         end
     end
     chat.PlaySound()
@@ -47,30 +46,24 @@ net.Receive("DarkRP_Chat", AddToChat)
 
 --[[---------------------------------------------------------------------------
 Credits
-
 Please only ADD to the credits.
 ---------------------------------------------------------------------------]]
 local creds =
 [[
-
 LightRP was created by Rick darkalonio. LightRP was sandbox with some added RP elements.
 LightRP was released at the end of January 2007
-
 DarkRP was created as a spoof of LightRP by Rickster, somewhere during the summer of 2007.
 Note: There was a DarkRP in 2006, but that was an entirely different gamemode.
-
 Rickster went to serve his country and went to Afghanistan. During that time, the following people updated DarkRP:
 Picwizdan
 Sibre
 [GNC] Matt
 PhilXYZ
 Chromebolt A.K.A. Unib5 (STEAM_0:1:19045957)
-
 In 2008, Unib5 was administrator on a DarkRP server called EuroRP, owned by Jiggu. FPtje frequently joined this server to prop kill en masse. While Jiggu loved watching the chaos unfold, Unib5 hated it and banned FPtje on sight. Since Jiggu kept unbanning FPtje, Unib5 felt powerless. In an attempt to stop FPtje, Unib5 put FPtje's favourite prop killing props (the locker and the sawblade) in the default blacklist of DarkRP in an update. This in turn enraged FPtje, as he swore to make an update in secret that would suddenly pop up and overthrow the established version. As a result, DarkRP 2.3.1 was released in December 2008. After a bit of a fight, FPtje became the official updater of DarkRP.
-
 Current developer:
     Falco A.K.A. FPtje Atheos (STEAM_0:0:8944068)
-
+    Nephew | Tesseract Servers
 People who have contributed (ordered by commits, with at least two commits)
     Bo98
     Drakehawke (STEAM_0:0:22342869) (64 commits on old SVN)
