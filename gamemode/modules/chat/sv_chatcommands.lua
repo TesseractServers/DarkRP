84c3e883d894a8c644b8f24b10b65316235c61d6
Tassilo <tassia710@gmail.com>
2020-12-23 19:58:05 +0100

    Removed /ooc
    Once again, this is conflicting with our own system.

--- gamemode/modules/chat/sv_chatcommands.lua
+++ gamemode/modules/chat/sv_chatcommands.lua
@@ -50,36 +50,6 @@ local function Me(ply, args)
 end
 DarkRP.defineChatCommand("me", Me, 1.5)
 
-local function OOC(ply, args)
-    if not GAMEMODE.Config.ooc then
-        DarkRP.notify(ply, 1, 4, DarkRP.getPhrase("disabled", DarkRP.getPhrase("ooc"), ""))
-        return ""
-    end
-
-    local DoSay = function(text)
-        if text == "" then
-            DarkRP.notify(ply, 1, 4, DarkRP.getPhrase("invalid_x", DarkRP.getPhrase("arguments"), ""))
-            return ""
-        end
-        local col = team.GetColor(ply:Team())
-        local col2 = Color(255, 255, 255, 255)
-        if not ply:Alive() then
-            col2 = Color(255, 200, 200, 255)
-            col = col2
-        end
-
-        local phrase = DarkRP.getPhrase("ooc")
-        local name = ply:Nick()
-        for _, v in ipairs(player.GetAll()) do
-            DarkRP.talkToPerson(v, col, "(" .. phrase .. ") " .. name, col2, text, ply)
-        end
-    end
-    return args, DoSay
-end
-DarkRP.defineChatCommand("/", OOC, true, 1.5)
-DarkRP.defineChatCommand("a", OOC, true, 1.5)
-DarkRP.defineChatCommand("ooc", OOC, true, 1.5)
-
 local function MayorBroadcast(ply, args)
     if args == "" then
         DarkRP.notify(ply, 1, 4, DarkRP.getPhrase("invalid_x", DarkRP.getPhrase("arguments"), ""))
